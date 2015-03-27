0917117184f1084f137544085b999a2c8a88b1f6
Kirill Demidov <kademidov@gmail.com>
2015-03-28 02:54:26 +0300

    init - finish

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(function($){
+   $(".phone-input").mask("+7 (999) 999-99-99");
+});
+
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+      infiniteLoop: true,
+      hideControlOnEnd: true,
+      adaptiveHeight: true,
+      pagerLocation: 'top',
+    });
+});
